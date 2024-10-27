ca44eb0c6af153744570135d82669065b12d1461
Alexsey779 <fktrctq321@gmail.com>
2024-10-27 15:56:23 +0700

    second commit

--- game1/game_mario/script.js
+++ game1/game_mario/script.js
@@ -1,48 +1,58 @@
+
+// вытаскиваем индификаторы из html
 const player = document.getElementById("player");
 const damper = document.getElementById("damper");
 const btn = document.querySelector(".start");
- let interval;
+
+let interval;
 let isStart = false;
+
 const activeJump = () => {
-if(isStart === true) {
- if(!player.classList.contains("active")){
-    player.classList.add("active");
- }
-    
- setTimeout(() => {
-    player.classList.remove("active");
-
- }, 300)
-}
-}
-
-const startGame = () => {
-isStart = true;
-console.log(parseInt(window.getComputedStyle(player).getPropertyValue('top')));
-damper.classList.add("animate")
-   
-interval = setInterval(() => {
-let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
-let damperLeft = parseInt(window.getComputedStyle(damper).getPropertyValue('left')) 
-if(damperLeft < 50 && damperLeft > 0 && playerTop >= 140){
-    andGame();
-    alert("Вы проиграли!")
-    
-}
-
-}, 10) 
-
-
-}
-
-const andGame = () => {
-    isStart = false;
-    clearInterval(interval); 
-    damper.classList.remove("animate")   
+  if (isStart === true) {
+    if (!player.classList.contains("active")) {
+      player.classList.add("active");
     }
 
-document.addEventListener("keydown", activeJump)
-btn.addEventListener("click", ()=>{
+    setTimeout(() => {
+      player.classList.remove("active");
+    }, 300);
+  }
+};
+// создаем функцию старта игры
+const startGame = () => {
+//   иницилизируем начальное значение переменной true
+    isStart = true; 
+//  добавляем анимацию через черепахи через метод
+  damper.classList.add("animate");
+// инициализируем переменную interval значением setInterval()
+  interval = setInterval(() => {
+    // обьявляем переменную и делаем проверку в марио методом и вычисляем целое число убирая буквы
+    let playerTop = parseInt(
+      window.getComputedStyle(player).getPropertyValue("top")
+    );
+    // то же самое только для черепахи
+    let damperLeft = parseInt(
+      window.getComputedStyle(damper).getPropertyValue("left")
+    );
+    // пишем проверку если черепаха ближе 50 px но больше 0 и марио находиться больше либо равно 140px то игра прекрашается
+    if (damperLeft < 50 && damperLeft > 0 && playerTop >= 140) {
+        // вызываем функцию остановки игры
+      andGame();
+    //   выводим запись на экран
+      alert("Вы проиграли!");
+    }
+  }, 10);
+};
+// создаем функцию для оканчания игры
+const andGame = () => {
+    // назначаем переменной значение false
+  isStart = false;
+//   вызываем функцию для очистки функции
+  clearInterval(interval);
+  damper.classList.remove("animate");
+};
 
-    startGame()
+document.addEventListener("keydown", activeJump);
+btn.addEventListener("click", () => {
+  startGame();
 });
